1ce17d82784c320d9e70d5e6acb93f193f00be8b
w3cui <tql163.love@163.com>
2019-01-08 17:56:59 +0800

    d

--- src/components/links/index.tsx
+++ src/components/links/index.tsx
@@ -61,20 +61,6 @@ export default class Index extends Component {
   render() {
     return (
       <View >
-        <View className='bg_layer'></View>
-        <View className='input_main'>
-          <View className='input_title'>
-            <View className='input_back' catchtap='hidePayLayer'><Text></Text></View>
-            <Text>输入支付密码</Text>
-          </View>
-          <View className='input_tip'><Text>使用会员卡余额支付需要验证身份，验证通过后才可进行支付。</Text></View>
-          <View className='input_row' catchtap='getFocus'>
-            <View className='pwd_item' wx: for='{{ 6}}' wx:key='item' wx:for-index='i'>
-                      <Text>fdsafdsa</Text>
-          </View>
-        </View>
-        <View className='forget_pwd' catchtap='hidePayLayer'>忘记密码</View>
-        <Input className='input_control' password type='number' focus='{{payFocus}}' bindinput='inputPwd' maxlength='6' />
       </View >
     )
   }
